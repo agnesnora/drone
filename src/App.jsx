16416bcae91d69b89c2ba4c51dbf8ee3bedb4fbd
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-01-16 11:47:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Section1 from "./section1";
+import drone from "../public/images/drone-transparent.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Section1 />
+    </>
+  );
+}
+
+export default App;
